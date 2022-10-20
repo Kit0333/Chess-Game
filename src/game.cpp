6f977d7599cdd60e38b1b7f08bd3ed1bda83ce67
Kit0333 <100954066+Kit0333@users.noreply.github.com>
2022-10-20 17:47:36 +0200

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -96,3 +96,37 @@ int& Game::mousePress(SDL_MouseButtonEvent& b){
     }
     return state;
 };
+
+
+Board::Board(){
+    int  boardGame[8][8]={                // Index:
+                        {4,3,2,5,6,2,3,4},//  0-7
+                        {1,1,1,1,1,1,1,1},//  8-15
+                        {0,0,0,0,0,0,0,0},//  16-23
+                        {0,0,0,0,0,0,0,0},//  24-31
+                        {0,0,0,0,0,0,0,0},//  32-39
+                        {0,0,0,0,0,0,0,0},//  40-47
+                        {1,1,1,1,1,1,1,1},//  48-55
+                        {4,3,2,6,5,2,3,4}};// 6-63
+    m_board=&boardGame[0][0];//Pointer to the first element
+
+};
+Board::~Board() {};
+
+Pawn::Pawn(int type,Player master){
+    m_master=master;
+    m_type=type;
+    m_life=true;
+
+    /*
+    ----Unit type information : 
+    1: Pawn
+    2: Bishop
+    3: The knight
+    4: Rook
+    5: Queen
+    6: King  
+    
+    */
+};
+Pawn::~Pawn(){};
