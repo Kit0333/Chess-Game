503bf6a48f386c2ae538cc5b4c2c5b15c1533312
Kit0333 <100954066+Kit0333@users.noreply.github.com>
2022-10-24 00:54:09 +0200

    Update game.hpp

--- include/headers/game.hpp
+++ include/headers/game.hpp
@@ -4,51 +4,58 @@
 #include <sdl2/SDL_image.h>
 
 enum class GameState{PLAY,EXIT};
+enum class Player{J1,J2};
 class Game{
 public:
-    Game(const char* title,Uint32 flags);
+    Game(const char* title,Uint32 flags,int &width,int &height);
     ~Game();
 
     SDL_Texture* loadTexture(const char* filePath);
-    void render(SDL_Texture* tex,int x=0,int y=0,int w=1920,int h=1080);
+    void render(SDL_Texture* tex,int x=0,int y=0,int w=1024,int h=600);
     GameState getGameState();
     int& mousePress(SDL_MouseButtonEvent& b);
-    void Game::gameStateChange(GameState x);
-         
+    void gameStateChange(GameState x);
+    
+    Player getActualPlayer();
+    void playerSwitch();
+
     void run();
+    void clear();
+    void display();
 
 private:
 
-    void clear();
-    void display();
     void cleanup();
 
     void UserScreenSize(int&width, int &height);
+    Player m_player;
     GameState m_gameState;
     SDL_Window* m_window;
     SDL_Renderer* m_renderer;
 };
-
-class Board{
-public:
-    Board();
-    ~Board();
-private:
-    int *m_board;
-    Pawn *m_pawn;
-    Pawn *m_bishop;
-    Pawn *m_knight;
-    Pawn *m_queen;
-    Pawn *m_king;
-};
-
-enum class Player{P1,P2};
 class Pawn{
 public:
     Pawn(int type);
     ~Pawn();
 
 private:
-    int m_type; 
+    int m_type;
     const char* m_info;
 };
+
+class Board{
+public:
+    Board();
+    ~Board();
+    int getBoardN(int i,int j);
+    int trace(int &x1,int&y1,int&x2,int&y2);
+private:
+    int *m_board;
+    Pawn *m_pawn;
+    Pawn *m_bishop;
+    Pawn *m_knight;
+    Pawn *m_rook;
+    Pawn *m_queen;
+    Pawn *m_king;
+};
+
